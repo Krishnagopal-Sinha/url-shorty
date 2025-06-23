afdf20cb47d9687c0c707880ee76d443283b577e
Krishnagopal-Sinha <ayushsinha24@gmail.com>
2025-06-23 22:42:06 +0530

    chore: FE setup

--- db/session.py
+++ db/session.py
@@ -1,10 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from typing import Generator
 from core.config import settings
 
 SQL_ALCHEMY_DATABASE_URL = settings.DATABASE_URL
-print('Databse URL is: ', SQL_ALCHEMY_DATABASE_URL)
+print("Databse URL is: ", SQL_ALCHEMY_DATABASE_URL)
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from fastapi import FastAPI
 from core.config import settings
 
 from db.session import engine
-from db.base_class import Base
+from db.base import Base
+from apis.base import api_router
 
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
 
+def include_router(app):
+    return app.include_router(api_router)
+
+
 def start_app():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     create_tables()
+    include_router(app)
     return app
 
 
